fa3739b453e8a5960cc7debda44e3259b45672fc
jyuwang <100096661+jyuwang@users.noreply.github.com>
2022-12-05 00:00:59 -0500

    completed version; works on 3.1 example

--- rsa.py
+++ rsa.py
@@ -2,33 +2,51 @@ import typing
 
 encryptedMessage = 163077576587089932277514178989798339755826189700674110151160860819557757512053108465634676999401755817425637794522932574265893488854028596522889419543378155476439015236106447427921542963150735762104095795184542
 
+
 def totient(P, Q):
     return (P-1) * (Q-1)
 
+
 def modularInverse(integer, modulo):
-    pass
+    # what is a modular inverse
+    # A * A^-1 .   mod x = 1
+    # A^-1 is an element of Zn
+    # Zn is a set of all numbers from 1 to n-1
+    # in this case n is psi(n), which just happens to be the modulo argument
+    for i in range(2, modulo):
+        if (integer * i) % modulo == 1:
+            return i
+
 
 def FME(base, exponent, modulo):
-    pass
+    result = 1
+    while exponent > 1:
+        if exponent & 1:
+            result = (result * base) % modulo
+        base = (base * base) % modulo
+        exponent >>= 1
+    return (base * result) % modulo
 
+
+def numToString(num):
+    alphabet = [' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+                'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    result = ''
+    while num > 0:
+        index = num % 27
+        num = num // 27
+        result = alphabet[index] + result
+    return result
 
 
 def decrypt(p: int, q: int, e: int, c: int) -> str:
-    print("beginning decryption...")
-
-    p=435958568325940791799951965387214406385470910265220196318705482144524085345275999740244625255428455944579
-    q=562545761726884103756277007304447481743876944007510545104946851094548396577479473472146228550799322939273
-    n=p*q
-    e=7
+    n = p*q
 
     psi = totient(p, q)
     inverseE = modularInverse(e, psi)
     m = FME(c, inverseE, n)
 
-    return m
-    
+    return numToString(m)
 
 
-decrypt(1, 2, 3, 4)
-
-
+# print("The original message was: ", decrypt(7, 11, 13, 5))
